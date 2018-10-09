3c10207f9c8def3c5ae0aa446778db44cfeb90e4
Luca Mcleod <luca.mcleod@mymail.barry.edu>
2018-10-08 21:09:07 -0400

    Update PrimeNumbers.py

---
+++ PrimeNumbers.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n < 2 :
+        return False
+    elif n == 2:
+        return True
+    else:
+        for x in range(2, n):
+            if n % x == 0:
+                return False
+    return True
+print(is_prime(int(input("Enter an integer:"))))
